f04e9c74822b3e7434fb956b8f2c190f0ed24e05
cliff <60359951+cliff82@users.noreply.github.com>
2023-03-06 15:42:29 -0500

    Add files via upload
    first commit

---
+++ build/precache-manifest.a8aebc4939f6845f207f2b4ac35fed03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa215c1740ee6294cb8b",
+    "url": "/static/css/main.2e977258.chunk.css"
+  },
+  {
+    "revision": "aa215c1740ee6294cb8b",
+    "url": "/static/js/main.d109ff3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5a9329006027d950f33",
+    "url": "/static/js/2.74b0dfdc.chunk.js"
+  },
+  {
+    "revision": "04611ba5332ef83a9062ac221f4b3256",
+    "url": "/index.html"
+  }
+];
